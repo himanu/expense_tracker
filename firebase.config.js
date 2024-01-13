c7da359551576bafa75e13a9343b3a6b419738a7
Himanshu Yadav <hy287719@gmail.com>
2024-01-13 19:07:51 +0530

    React 17

---
+++ firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAaWOflD1Hg4VmQJSvlqtdsiIi1HcdNbnk",
+    authDomain: "fir-384a7.firebaseapp.com",
+    databaseURL: "https://fir-384a7-default-rtdb.firebaseio.com",
+    projectId: "fir-384a7",
+    storageBucket: "fir-384a7.appspot.com",
+    messagingSenderId: "156594093226",
+    appId: "1:156594093226:web:a369d063bb10e994a8a4d3",
+    measurementId: "G-XZG47XR2V9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import {useState} from "react";
 function App() {
+  const [user, setUser] = useState('');
+  
   return (
     <div className="text-white text-center m-auto pt-[5%] bg-gradient-to-r from-gray-700 via-gray-800 to-gray-700 h-screen">
       <div className="text-3xl">
