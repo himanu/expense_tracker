441e667fbfe9d8adb7cb8b1aac2a8555aa5d0ecc
Himanshu Yadav <himanshu.yadav@aknamed.com>
2024-01-13 18:44:12 +0530

    Welcome page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-white text-center m-auto pt-[5%] bg-gradient-to-r from-gray-700 via-gray-800 to-gray-700 h-screen">
+      <div className="text-3xl">
+        Welcome to Expense Tracker !
+      </div>
+      <div className="mt-4 text-md">
+        Add, View, Edit and Delete your expenses.
+      </div>
+      <button className="text-sm mt-5 hover:bg-gradient-to-l from-purple-700 via-purple-900 to-purple-700 bg-gradient-to-r from-purple-900 via-purple-700 to-purple-900  py-1 px-3 rounded-md">
+        Sign In / Log In
+      </button>
     </div>
   );
 }
